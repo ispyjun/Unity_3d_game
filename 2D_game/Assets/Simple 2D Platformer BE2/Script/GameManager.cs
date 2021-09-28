6e26946393df88d5b8788560321dd1a0318941dc
ispyjun <ispyjun@gmail.com>
2021-09-28 16:21:57 +0900

    2D_Game
    2D_Game

---
+++ 2D_game/Assets/Simple 2D Platformer BE2/Script/GameManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int totalPoint;
+    public int stagePoint;
+    public int stageIndex;
+    public int health;
+    public PlayerMove player;
+    public GameObject[] Stages;
+
+    public Image[] UIhealth;
+    public Text UIPoint;
+    public Text UIStage;
+    public GameObject UIRestartBtn;
+
+    void Update()
+    {
+        UIPoint.text = (totalPoint + stagePoint).ToString();
+    }
+
+    public void NextStage()
+    {
+        if (stageIndex < Stages.Length - 1)
+        {
+            Stages[stageIndex].SetActive(false);
+            stageIndex++;
+            Stages[stageIndex].SetActive(true);
+            PlayerReposition();
+
+            UIStage.text = "STAGE" + (stageIndex + 1);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            Debug.Log("클리어!");
+
+            Text btnText = UIRestartBtn.GetComponentInChildren<Text>();
+            btnText.text = "Clear!";
+            UIRestartBtn.SetActive(true);
+        }
+
+        totalPoint += stagePoint;
+        stagePoint = 0;
+    }
+
+    public void HealthDown()
+    {
+        if (health > 1) {
+            health--;
+            UIhealth[health].color = new Color(1, 0, 0, 0.4f);
+        }
+        else
+        {
+            UIhealth[0].color = new Color(1, 0, 0, 0.4f);
+            player.OnDie();
+            Debug.Log("죽었습니다.");
+
+            UIRestartBtn.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (health > 1)
+            {
+                PlayerReposition();
+            }
+
+            HealthDown();
+        }
+    }
+
+    public void PlayerReposition()
+    {
+        player.transform.position = new Vector3(0, 0, -1);
+        player.VelocityZero();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
