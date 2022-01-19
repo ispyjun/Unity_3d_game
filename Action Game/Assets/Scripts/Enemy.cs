c284ab971594bc89c1da7b89b8335de281c972cc
ispyjun <ispyjun@gmail.com>
2022-01-19 22:57:55 +0900

    ActionGame
    8day
    AI

--- Action Game/Assets/Scripts/Enemy.cs
+++ Action Game/Assets/Scripts/Enemy.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public int maxHealth;
     public int curHealth;
+    public Transform target;
 
     Rigidbody rigid;
     BoxCollider boxCollider;
     Material mat;
+    NavMeshAgent nav;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
-        mat = GetComponent<MeshRenderer>().material;
+        mat = GetComponentInChildren<MeshRenderer>().material;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        nav.SetDestination(target.position);
     }
 
     void OnTriggerEnter(Collider other)
